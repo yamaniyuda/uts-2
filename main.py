4e6a4c6cf0aa2d9c46f60e2068ebc9c462da4b62
yamani59 <yamaniyud@gmail.com>
2024-05-05 09:13:55 +0700

    feat: add date repository

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ import data.data
 
 
 def initial() -> None:
-    root_menu = apps.show_data_menu.main()
-    # while True:
-    #     root_menu: str = None
+    while True:
+        root_menu: str = None
 
-    #     match root_menu:
-    #         case '1': root_menu = apps.add_menu.main()
-    #         case '2': root_menu = apps.show_data_menu.main()
-    #         case '3': root_menu = apps.update_menu.main()
-    #         case '4': root_menu = apps.sort_menu.main()
-    #         case '5': root_menu = apps.search_menu.main()
-    #         case '6': break
-    #         case  _ : root_menu = apps.main_menu.main()
+        match root_menu:
+            case '1': root_menu = apps.add_menu.main()
+            case '2': root_menu = apps.show_data_menu.main()
+            case '3': root_menu = apps.update_menu.main()
+            case '4': root_menu = apps.sort_menu.main()
+            case '5': root_menu = apps.search_menu.main()
+            case '6': break
+            case  _ : root_menu = apps.main_menu.main()
 
 
 if __name__ == "__main__":
